afe135bac1aa68684d9f81bd76cde5f3c198d1c8
thewildvegetable <whichazelnj13@aol.com>
2019-07-21 18:57:09 -0400

    no 404

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use('/assets', express.static(path.resolve(`${__dirname}/app/dist/test`)));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(`${__dirname}/app/dist/test/index.html`));
 });
 
